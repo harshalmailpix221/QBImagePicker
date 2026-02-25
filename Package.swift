240a4eda158c582198c9b5fd03d9cbcfb9356acb
harshalmailpix221 <harshal@1hourphoto.com>
2026-02-25 13:37:46 -0600

    added package.siwft to import this using SPM

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+name: "QBImagePicker",
+platforms: [
+.iOS(.v11)
+],
+products: [
+.library(
+name: "QBImagePicker",
+targets: ["QBImagePicker"]
+)
+],
+targets: [
+.target(
+name: "QBImagePicker",
+path: "QBImagePicker"
+)
+]
+)
