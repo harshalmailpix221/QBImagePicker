76f7be838d89ae02b968397f5c1a795ec67bba58
harshalmailpix221 <harshal@1hourphoto.com>
2026-02-25 15:45:03 -0600

    restructuring for Swift package. adjustments.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     .target(
     	    name: "QBImagePicker",
-    	    path: "Sources/QBImagePicker",
+    	    path: "QBImagePicker",
     	    publicHeadersPath: "."
 	)	
 )
