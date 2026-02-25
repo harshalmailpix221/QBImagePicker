1d5553ffb0a505438744eee63488d81223b5684e
mailpix-developer <152657382+mailpix-developer@users.noreply.github.com>
2026-02-25 14:39:06 -0600

    Reformatted Package.swift

--- Package.swift
+++ Package.swift
@@ -2,20 +2,20 @@
 import PackageDescription
 
 let package = Package(
-name: "QBImagePicker",
-platforms: [
-.iOS(.v11)
-],
-products: [
-.library(
-name: "QBImagePicker",
-targets: ["QBImagePicker"]
-)
-],
-targets: [
-.target(
-name: "QBImagePicker",
-path: "QBImagePicker"
-)
-]
+    name: "QBImagePicker",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "QBImagePicker",
+            targets: ["QBImagePicker"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "QBImagePicker",
+            path: "QBImagePicker"
+        )
+    ]
 )
